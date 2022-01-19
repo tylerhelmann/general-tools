7c31f8fe8ea6525eaf7e1304ff00d745acc7cf64
Tyler Helmann <tch54@cornell.edu>
2022-01-19 16:21:09 -0500

    fix bash link

--- save_cp_pep_sh.R
+++ save_cp_pep_sh.R
@@ -18,7 +18,7 @@ print_faa_cp_command <- function(row, strainlist){
 save_faa_cp_command <- function(strainlist, file_name){
   commands <- c(sapply(c(1:nrow(strainlist)), print_faa_cp_command,
                        strainlist = strainlist))
-  commands <- c("#! bash \n", commands)
+  commands <- c("#! /bin/bash \n", commands)
   write.table(commands, file = file_name, 
               row.names = F, quote = F, col.names = F)
 }
